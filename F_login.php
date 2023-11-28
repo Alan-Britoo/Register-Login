1d813c954677780b0f2e559f779c3ea0d8473195
Alan-Britoo <rotcarrillo_8@hotmail.com>
2023-11-28 13:04:13 -0500

    2vistas

---
+++ F_login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/F_login.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="img">
+            <img src="../MP_N3-master/assets/devchallenges.svg" alt="icono de arriba">
+        </div>
+
+        <P class="pri">Login </P>
+        <div class="emaill">
+            <i class="material-symbols-outlined">Email</i>
+        <input type="text" class="email"
+                placeholder="Email">
+        </div>
+        <div class="contra">
+            <i class="material-symbols-outlined">lock</i>
+            <input type="password" class="contraseña" placeholder="Password">
+        </div>
+        <button class="start">Login</button><br>
+        <p class="ultimo">or continue with these social profile</p>
+        <div class="iconos">
+            <img src="../MP_N3-master/assets/Google.svg" alt="">
+            <img src="../MP_N3-master/assets/Facebook.svg" alt="">
+            <img src="../MP_N3-master/assets/Twitter.svg" alt="">
+            <img src="../MP_N3-master/assets/Gihub.svg" alt="">
+        </div>
+        <p class="ultimo">Don’t have an account yet? <a href=""> Register</a></p>
+        
+    </div>
+    <div class="refer">
+        <p>created by RotsenC.</p>
+        <p>devChallenges.io</p>
+    </div>
+
+</body>
+
+</html>
---
+++ F_register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="img">
+            <img src="../MP_N3-master/assets/devchallenges.svg" alt="icono de arriba">
+        </div>
+
+        <P class="pri">Join thousands of learners from around the world </P>
+        <P class="seg">Master web development by making real-life projects. There are multiple paths for you to choose
+        </P>
+        <div class="emaill">
+            <i class="material-symbols-outlined">Email</i>
+        <input type="text" class="email"
+                placeholder="Email">
+        </div>
+        <div class="contra">
+            <i class="material-symbols-outlined">lock</i>
+            <input type="password" class="contraseña" placeholder="Password">
+        </div>
+        <button class="start">Start coding now </button><br>
+        <p class="ultimo">or continue with these social profile</p>
+        <div class="iconos">
+            <img src="../MP_N3-master/assets/Google.svg" alt="">
+            <img src="../MP_N3-master/assets/Facebook.svg" alt="">
+            <img src="../MP_N3-master/assets/Twitter.svg" alt="">
+            <img src="../MP_N3-master/assets/Gihub.svg" alt="">
+        </div>
+        <p class="ultimo">Adready a member? <a href=""> Login</a></p>
+        
+    </div>
+    <div class="refer">
+        <p>created by RotsenC.</p>
+        <p>devChallenges.io</p>
+    </div>
+
+</body>
+
+</html>
